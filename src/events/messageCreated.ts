32c77e20902b5057cb930f164ef9269543d5553f
BeneX Drake <50136177+Benexdrake@users.noreply.github.com>
2023-07-29 21:05:49 +0200

    cleaning

--- src/events/messageCreated.ts
+++
@@ -1,9 +0,0 @@
-import { client } from "..";
-import { Event } from "../client/Event";
-
-const {discord}  = require('../../config.json');
-
-export default new Event("messageCreate", async (message) => {
-
-
-});
--- src/events/welcome.ts
+++
@@ -1,7 +0,0 @@
-import { Event } from "../client/Event";
-import { WelcomeLogic } from "../logic/welcomeLogic";
-
-export default new Event("guildMemberUpdate", (oldMember,newMember) => 
-{
-    new WelcomeLogic().Welcome(newMember);
-});
